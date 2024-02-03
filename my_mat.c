a45a6f139d7d47e0eccea7fd0296d8c55c788e1c
Anthonios Deeb <anthonisdb@gmail.com>
2024-02-03 14:08:36 +0200

    testing

--- my_mat.c
+++ my_mat.c
@@ -34,41 +34,21 @@ bool FindShortestPath(int s,int d){
     return dist[s][d]!=0;
 }
 
-void printPath(int path[ROWS][COLS], int i, int j) {
-    if (path[i][j] == -1) {
-        printf("%d\n", -1);
-        return;
-    }
-    printPath(path, i, path[i][j]);
-    printf("%d\n", j);
-}
-
 void PrintShortestPath(int s,int d) {
-    int dist[ROWS][COLS];
-    int path[ROWS][COLS];
-
+   int dist[ROWS][COLS];
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
             dist[i][j] = mat[i][j];
-             if (i == j) {
-                path[i][j] = -1;
-            } else if (dist[i][j] != 0) {
-                path[i][j] = i;
-            } else {
-                path[i][j] = -1;
-            }
         }
     }
-    
     for (int k = 0; k < ROWS; k++) {
-        for (int i = 0; i < COLS; i++) {
-            for (int j = 0; j < ROWS; j++) {
-                if (dist[i][k] != 0 && dist[k][j] != 0 && (dist[i][j] == 0 || dist[i][k] + dist[k][j] < dist[i][j])) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
-                    path[i][j] = k;
+        for (int x = 0; x < COLS; x++) {
+            for (int y = 0; y < ROWS; y++) {
+                if (dist[x][k] != 0 && dist[k][y] != 0 && (dist[x][y] == 0 || dist[x][k] + dist[k][y] < dist[x][y])) {
+                    dist[x][y] = dist[x][k] + dist[k][y];
                 }
             }
         }
     }
-    printPath(path,s,d);
+    printf("%d",dist[i][j]);
 }
